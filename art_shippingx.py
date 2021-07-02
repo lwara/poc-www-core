431bda708eb4bc9341b17f35a883d8518464c7c2
Tiwonge Lwara <43346571+lwara@users.noreply.github.com>
2021-07-02 15:13:13 +0200

    Update art_shippingx.py
    Indentation 2

--- art_shippingx.py
+++ art_shippingx.py
@@ -1,9 +1,10 @@
-import requests
+ import requests
 import json
 import platform
 import subprocess
 import os
 from fabric import Connection
+
 
 def get_xi_data(url):
     response = requests.get(url)
@@ -11,15 +12,19 @@ def get_xi_data(url):
     data = data[0]['fields']
     return data
 
+
 """ 
 * sends SMS alerts
 * @params url, params
 * return dict
 """
+
+
 def alert(url, params):
     headers = {'Content-type': 'application/json; charset=utf-8'}
     r = requests.post(url, json=params, headers=headers)
     return r
+
 
 recipients = ["+265998006237", "+265991450316", "+265995246144", "+265992182669"]
 
@@ -31,42 +36,46 @@ for site_id in cluster['site']:
     # functionality for ping re-tries
     count = 0
 
-    while(count < 3):
+    while (count < 3):
 
         # lets check if the site is available
-        param = '-n' if platform.system().lower()=='windows' else '-c'
+        param = '-n' if platform.system().lower() == 'windows' else '-c'
         if subprocess.call(['ping', param, '1', site['ip_address']]) == 0:
-            
+
             # ship data to remote site
-            push_art = "rsync " + "-r $WORKSPACE/BHT-Core/apps/ART/ " + site['username'] + "@" + site['ip_address'] + ":/var/www/BHT-Core/apps"
+            push_art = "rsync " + "-r $WORKSPACE/BHT-Core/apps/ART/ " + site['username'] + "@" + site[
+                'ip_address'] + ":/var/www/BHT-Core/apps"
             os.system(push_art)
 
             # run setup script
-            run_api_script = "ssh " + site['username'] + "@" + site['ip_address'] + " 'cd /var/www/BHT-Core && ./core_art_setup.sh'"
+            run_api_script = "ssh " + site['username'] + "@" + site[
+                'ip_address'] + " 'cd /var/www/BHT-Core && ./core_art_setup.sh'"
             os.system(run_api_script)
+            result = Connection("" + site['username'] + "@" + site['ip_address'] + "").run(
+                'cd /var/www/BHT-Core/apps/ART && git describe', hide=True)
+            msg = "{0.stdout}"
 
-	    result = Connection("" + site['username'] + "@" + site['ip_address'] + "").run('cd /var/www/BHT-Core/apps/ART && git describe', hide=True)
-	    
-	    msg = "{0.stdout}"
-            
             version = msg.format(result).strip()
-            
+
             api_version = "v4.12.0"
-            
+
             if api_version == version:
-                msgx = "Hi there,\n\nDeployment of ART to " + version + " for " + site['name'] + " completed succesfully.\n\nThanks!\nEGPAF HIS."
+                msgx = "Hi there,\n\nDeployment of ART to " + version + " for " + site[
+                    'name'] + " completed succesfully.\n\nThanks!\nEGPAF HIS."
             else:
-                msgx = "Hi there,\n\nSomething went wrong while checking out to the latest ART version. Current version is " + version + " for " + site['name'] + ".\n\nThanks!\nEGPAF HIS."
+                msgx = "Hi there,\n\nSomething went wrong while checking out to the latest ART version. Current version is " + version + " for " + \
+                       site['name'] + ".\n\nThanks!\nEGPAF HIS."
 
             # send sms alert
             for recipient in recipients:
- 	        msg = "Hi there,\n\nDeployment of ART to " + version + " for " + site['name'] + " completed succesfully.\n\nThanks!\nEGPAF HIS."
+                msg = "Hi there,\n\nDeployment of ART to " + version + " for " + site[
+                    'name'] + " completed succesfully.\n\nThanks!\nEGPAF HIS."
                 params = {
                     "tenant_id": "12345",
                     "recipient": recipient,
                     "message": msg,
                     "message_category": "signup",
-                    "brand_name": "EGPAF-HIS",    
+                    "brand_name": "EGPAF-HIS",
                     "type": "internal"
                 }
                 alert("http://ec2-52-14-138-182.us-east-2.compute.amazonaws.com:56733/v1/sms/send", params)
@@ -78,13 +87,14 @@ for site_id in cluster['site']:
             # make sure we are sending the alert at the last pint attempt
             if count == 3:
                 for recipient in recipients:
-                    msg = "Hi there,\n\nDeployment of ART to v4.12.0 for " + site['name'] + " failed to complete after several connection attempts.\n\nThanks!\nEGPAF HIS."
+                    msg = "Hi there,\n\nDeployment of ART to v4.12.0 for " + site[
+                        'name'] + " failed to complete after several connection attempts.\n\nThanks!\nEGPAF HIS."
                     params = {
                         "tenant_id": "12345",
                         "recipient": recipient,
                         "message": msg,
                         "message_category": "signup",
-                        "brand_name": "EGPAF-HIS",    
+                        "brand_name": "EGPAF-HIS",
                         "type": "internal"
                     }
                     alert("http://ec2-52-14-138-182.us-east-2.compute.amazonaws.com:56733/v1/sms/send", params)
